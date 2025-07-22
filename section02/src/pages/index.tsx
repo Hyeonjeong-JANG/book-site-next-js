a19475b6cfe8c28521393e788add1881ff9062ad
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2025-07-22 14:40:25 +0900

    SEO 설정

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,4 +1,3 @@
-// CSS Module
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
 import { ReactNode } from "react";
@@ -6,13 +5,9 @@ import BookItem from "@/components/book-item";
 import { InferGetStaticPropsType } from "next";
 import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-random-books";
+import Head from "next/head";
 
 export const getStaticProps = async () => {
-  // const allBooks = await fetchBooks();
-  // const recoBooks = await fetchRandomBooks();
-  // 위의 코드는 API 요청이 직렬로 되기 때문에 allBooks 데이터를 받고 나서 recoBooks를 받음
-  // 병렬로 바꾸면 한 번에 된다. 아래를 보라.
-  console.log('인덱스 페이지');
   const [allBooks, recoBooks] = await Promise.all([
     fetchBooks(),
     fetchRandomBooks(),
@@ -27,16 +22,27 @@ export const getStaticProps = async () => {
 export default function Home({ allBooks, recoBooks }: InferGetStaticPropsType<typeof getStaticProps>) {
 
   return (
-    <div className={style.container}>
-      <section>
-        <h3>지금 추천하는 도서</h3>
-        {recoBooks.map((book) => <BookItem key={book.id} {...book} />)}
-      </section>
-      <section>
-        <h3>등록된 모든 도서</h3>
-        {allBooks.map((book) => <BookItem key={book.id} {...book} />)}
-      </section>
-    </div>
+    <>
+      <Head>
+        <title>한입북스</title>
+        <meta property="og:image" content="/thumbnaill.png" />
+        <meta property="og:title" content="한입북스" />
+        <meta
+          property="og:description"
+          content="한입 북스에 등록된 도서들을 만나보세요"
+        />
+      </Head>
+      <div className={style.container}>
+        <section>
+          <h3>지금 추천하는 도서</h3>
+          {recoBooks.map((book) => <BookItem key={book.id} {...book} />)}
+        </section>
+        <section>
+          <h3>등록된 모든 도서</h3>
+          {allBooks.map((book) => <BookItem key={book.id} {...book} />)}
+        </section>
+      </div>
+    </>
   );
 }
 
