6d481fa2d47765e0e34258e2ab1623d085f125e4
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2025-07-22 16:45:11 +0900

    App Router 프로젝트 세팅, 페이지 라우팅 설정하기

---
+++ section03/src/app/book/[id]/page.tsx
@@ -0,0 +1,10 @@
+import { disconnect } from "process";
+
+export default async function Page({
+    params
+}: {
+    params: Promise<{ id: string }>;
+}) {
+    const { id } = await params;
+    return <div>book/[id] page입니다: {id}</div>;
+}
