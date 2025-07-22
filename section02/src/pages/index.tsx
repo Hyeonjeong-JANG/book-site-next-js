83b1d1cc78a02d9a85e22d4b11fdb20e1f827de4
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2025-07-22 14:01:49 +0900

    On-Demand ISR

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -3,15 +3,16 @@ import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
 import { ReactNode } from "react";
 import BookItem from "@/components/book-item";
-import { InferGetServerSidePropsType } from "next";
+import { InferGetStaticPropsType } from "next";
 import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-random-books";
 
-export const getServerSideProps = async () => {
+export const getStaticProps = async () => {
   // const allBooks = await fetchBooks();
   // const recoBooks = await fetchRandomBooks();
   // 위의 코드는 API 요청이 직렬로 되기 때문에 allBooks 데이터를 받고 나서 recoBooks를 받음
   // 병렬로 바꾸면 한 번에 된다. 아래를 보라.
+  console.log('인덱스 페이지');
   const [allBooks, recoBooks] = await Promise.all([
     fetchBooks(),
     fetchRandomBooks(),
@@ -23,7 +24,7 @@ export const getServerSideProps = async () => {
     },
   };
 };
-export default function Home({ allBooks, recoBooks }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function Home({ allBooks, recoBooks }: InferGetStaticPropsType<typeof getStaticProps>) {
 
   return (
     <div className={style.container}>
