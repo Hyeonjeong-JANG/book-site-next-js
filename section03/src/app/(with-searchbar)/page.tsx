e04b17ce4071bc6c4fcd5b5c44431dd6c491a641
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2025-07-23 14:42:05 +0900

    네비게이팅

--- section03/src/app/(with-searchbar)/page.tsx
+++ section03/src/app/(with-searchbar)/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import styles from "./page.module.css";
-import ClientComponent from "./client-component";
-import ServerComponent from "./server-component";
+import ClientComponent from "../components/client-component";
+import ServerComponent from "../components/server-component";
 
 export default function Home() {
   return (
