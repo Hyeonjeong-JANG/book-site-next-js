34cfe344760449f98cb5a3186c2f7acb548edb33
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2025-07-24 18:49:55 +0900

    스켈레톤

--- section06/src/app/(with-searchbar)/search/page.tsx
+++ section06/src/app/(with-searchbar)/search/page.tsx
@@ -1,4 +1,5 @@
 import BookItem from "@/components/book-item";
+import BookListSkeleton from "@/components/skeleton/book-list-skeleton";
 import { BookData } from "@/types";
 import { delay } from "@/util/delay";
 import { Suspense } from "react";
@@ -38,7 +39,7 @@ export default async function Page({
   searchParams: { q?: string; };
 }) {
   return (
-    <Suspense key={searchParams.q} fallback={<div>Loading...</div>}>
+    <Suspense key={searchParams.q} fallback={<BookListSkeleton count={3}/>}>
       <SearchResult q={searchParams.q || ""} />
     </Suspense>
   );
