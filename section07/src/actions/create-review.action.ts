2f1de3c8e0399ce909c91f35ad78c3f54eb9bf91
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2025-07-25 15:37:30 +0900

    재검증 방식 5가지(주소, 경로, 레이아웃, 모든 데이터, 태그기준)

--- section07/src/actions/create-review.action.ts
+++ section07/src/actions/create-review.action.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { revalidatePath } from "next/cache";
+import { revalidatePath, revalidateTag } from "next/cache";
 
 export async function createReviewAction(formData: FormData) {
     const bookId = formData.get("bookId")?.toString();
@@ -23,7 +23,21 @@ export async function createReviewAction(formData: FormData) {
             }
         );
         console.log(response.status);
-        revalidatePath(`/book/${bookId}`);
+
+        // // 1. 특정 주소에 해당하는 페이지만 재검증
+        // revalidatePath(`/book/${bookId}`);
+
+        // // 2. 특정 경로의 모든 동적 페이지를 재검증
+        // revalidatePath("/book/[id]", "page");
+
+        // // 3. 특정 레이아웃을 갖는 모든 페이지를 재검증
+        // revalidatePath("/(with-searchbar)", "layout");
+
+        // // 4. 모든 데이터 재검증
+        // revalidatePath("/", "layout");
+
+        // 5. 태그 기준, 데이터 캐시 재검증: 이게 가장 좋음. 왜냐하면 다른 것들은 그 페이지의 모든 데이터 캐시를 새로고침 하는데 이 방법은 리뷰 데이터 캐시만 딱 삭제해서 훨씬 경제적임.
+        revalidateTag(`review-${bookId}`);
     } catch (err) {
         console.error(err);
         return;
--- section07/src/app/book/[id]/page.tsx
+++ section07/src/app/book/[id]/page.tsx
@@ -8,7 +8,8 @@ import ReviewEditor from "@/components/review-editor";
 async function BookDetail({ bookId }: { bookId: string }) {
 
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${bookId}`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${bookId}`,
+    { next: { tags: [`review-${bookId}`] } }
   );
   if (!response.ok) {
     if (response.status === 404) {
