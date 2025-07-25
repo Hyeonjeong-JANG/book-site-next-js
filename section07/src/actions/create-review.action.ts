daac1bd5314631ac5032ce7a1979447d336c3d47
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2025-07-25 16:39:07 +0900

    클라리언트 컴포넌트에서 서버액션 + 에러처리

--- section07/src/actions/create-review.action.ts
+++ section07/src/actions/create-review.action.ts
@@ -1,20 +1,25 @@
 "use server";
 
+import { delay } from "@/util/delay";
 import { revalidatePath, revalidateTag } from "next/cache";
 
-export async function createReviewAction(formData: FormData) {
+export async function createReviewAction(
+    _: any,
+    formData: FormData
+) {
     const bookId = formData.get("bookId")?.toString();
     const content = formData.get("content")?.toString();
     const author = formData.get("author")?.toString();
 
-    console.log(bookId, content, author);
-
-
     if (!bookId || !content || !author) {
-        return;
+        return {
+            status: false,
+            error: "리뷰 내용과 작성자를 입력해주세요",
+        };
     }
 
     try {
+        await delay(2000);
         const response = await fetch(
             `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,
             {
@@ -22,7 +27,10 @@ export async function createReviewAction(formData: FormData) {
                 body: JSON.stringify({ bookId, content, author }),
             }
         );
-        console.log(response.status);
+
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
 
         // // 1. 특정 주소에 해당하는 페이지만 재검증
         // revalidatePath(`/book/${bookId}`);
@@ -38,8 +46,14 @@ export async function createReviewAction(formData: FormData) {
 
         // 5. 태그 기준, 데이터 캐시 재검증: 이게 가장 좋음. 왜냐하면 다른 것들은 그 페이지의 모든 데이터 캐시를 새로고침 하는데 이 방법은 리뷰 데이터 캐시만 딱 삭제해서 훨씬 경제적임.
         revalidateTag(`review-${bookId}`);
+        return {
+            status: true,
+            error: "",
+        };
     } catch (err) {
-        console.error(err);
-        return;
+        return {
+            status: false,
+            error: `리뷰 저장에 실패했습니다: ${err}`,
+        };
     }
 }
